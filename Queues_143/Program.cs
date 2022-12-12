6c329f5d217419ae94c6cd860735d3d7cf6c45da
overol1221 <fiqiachmad12@gmail.com>
2022-12-12 16:38:26 +0700

    Create class program

--- Queues_143/Program.cs
+++ Queues_143/Program.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace Queues_143
 {
-    internal class Program
+
+    class Program
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+        public Program()
+        {
+            /*intializing the values of the variables REAR and FRONT to -1 to indicate that * the queue is intializing empty.*/
+            FRONT =-1;
+            REAR =-1;
+        }
+        public void insert(int element)
+        {
+
+        }
         static void Main(string[] args)
         {
         }
