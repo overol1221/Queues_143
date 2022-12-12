dd974f3e54dc9c854f5b21694a16c0bb922efa1b
overol1221 <fiqiachmad12@gmail.com>
2022-12-12 17:23:45 +0700

    Make  queue to the last position of array

--- Queues_143/Program.cs
+++ Queues_143/Program.cs
@@ -88,9 +88,20 @@ namespace Queues_143
                 /* traverses the queue till the last element present is an array*/
                 while (FRONT_position <= REAR_position)
                 {
-
+                    Console.Write(queue_array[FRONT_position] + "  ");
+                    FRONT_position++;
                 }
-
+                Console.WriteLine();
+            }
+            else
+            {
+                /* traverses the queue till the last position of the array */
+                while(FRONT_position <= max - 1)
+                {
+                    Console.Write(queue_array[FRONT_position] + "  ");
+                    FRONT_position++;
+                }
+                /**/
             }
         }
 
